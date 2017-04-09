305ebd91448c0aad07c47eb2175f23bdbd0decd3
wswiatkowski <werty240@gmail.com>
2017-04-09 13:13:05 +0200

    Created update_user and create_user methods

---
+++ DbConnection.py
@@ -0,0 +1,69 @@
+import psycopg2
+from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
+
+USER = 'evojam'
+DBNAME = 'test1'
+PASSWORD = ''
+TABLE = 'invitees'
+
+
+class DbManager:
+    def __init__(self):
+        try:
+            conn = psycopg2.connect(dbname=DBNAME, user=USER, password=PASSWORD)
+
+        except psycopg2.OperationalError:
+            raise RuntimeError('Wrong DB credentials, check configuration.')
+
+        conn.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+        self.cursor = conn.cursor()
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.cursor.close()
+
+    def fetch_user(self, name, email):
+        self.cursor.execute(
+            "SELECT * FROM {table} WHERE email = '{email}' AND invitee = '{name}';".format(table=TABLE,
+                                                                                           email=email,
+                                                                                           name=name))
+        return self.cursor.fetchall()
+
+    def create_user(self, name, email):
+        self.cursor.execute("INSERT INTO {table} VALUES ('{name}', '{email}')".format(table=TABLE, name=name,
+                                                                                      email=email))
+        return self.fetch_user(name, email)
+
+    def update_user(self, name, email):
+        self.cursor.execute("SELECT count(*) FROM {table} WHERE invitee = '{name}';".format(table=TABLE, name=name))
+        names = int(self.cursor.fetchall()[0][0])
+
+        self.cursor.execute("SELECT count(*) FROM {table} WHERE email = '{email}';".format(table=TABLE, email=email))
+        emails = int(self.cursor.fetchall()[0][0])
+
+        if emails + names > 2:
+            self.cursor.execute(
+                "SELECT * FROM {table} WHERE email = '{email}' OR invitee = '{name}';".format(table=TABLE,
+                                                                                              email=email,
+                                                                                              name=name))
+            conflicts = self.cursor.fetchall()
+
+            raise NameError(
+                "Conflicting invitee data, DELETE conflicting users: {conflicts}".format(conflicts=conflicts))
+
+        elif names > emails:
+            self.cursor.execute("UPDATE {table} SET email = '{email}' WHERE invitee = '{name}';".format(table=TABLE,
+                                                                                                        email=email,
+                                                                                                        name=name))
+
+        elif emails > names:
+            self.cursor.execute("UPDATE {table} SET invitee = '{name}' WHERE email = '{email}';".format(table=TABLE,
+                                                                                                        email=email,
+                                                                                                        name=name))
+        else:
+            return self.create_user(name, email)
+
+        return self.fetch_user(name, email)
+
+
+dbconn = DbManager()
+print dbconn.create_user('mlfwfwefwn2', 'mlninteergenfwwefet.pl')
